eaad7e23a06f57c960cbe9dd272cacd51e52d055
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-01-06 13:23:37 -0400

    Fix: Corregido algunos modelos y controladores para obtener la data de los chats y sus mensajes

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,116 +1,104 @@
-const Message = require('../models/Message');
-const User = require('../models/User');
+const Message = require('../models/Message')
+const User = require('../models/User')
 
 const get_msgByChat = async (req, res, next) => {
-    try{
-            const {chat} = req.params
+  try {
+    const { chat } = req.params
 
-            const msgs = await Message.find({
-                chat
-            });
-        
-            (msgs.length !== 0) ? res.json(msgs) : res.status(400).json({ msg: "Doesn't exist any messages from this chat, maybe the chat_id doesn't correct"})
+    const msgs = await Message.find({
+      chat,
+    })
 
-    } catch(error){
-        next(error);
-    };
-};
-
+    res.json(msgs)
+  } catch (error) {
+    next(error)
+  }
+}
 
 const createMsg = async (req, res, next) => {
+  try {
+    const { chat, user, message, parent } = req.body
 
-    try{
-        const {
-            chat, 
-            user, 
-            message, 
-            parent
-        } = req.body
+    const msg = new Message({
+      chat,
+      user,
+      message,
+      parent,
+    })
 
-        const msg = new Message({
-            chat, 
-            user, 
-            message, 
-            parent
-        })
+    // await Message.aggregate({
+    //   $lookup: {
+    //     from: User,
+    //     localfield: user,
+    //     foreignField: _id,
+    //     as: sender,
+    //   },
+    // })
+    //
+    msg.save((err, data) => {
+      if (err) return res.status(400).json(err)
+      console.log('new Message created', data)
 
-        await Message.aggregate(
-            
-                {
-                    $lookup:
-                    {
-                       from: User,
-                       localfield: user,
-                       foreignField: _id,
-                       as: sender
-                    }
-                }      
-            
-        )
-        
-        await msg.save((err, data) => {
-            if(err) return res.status(400).json(err)
-
-            return res.json({msg: "message succesfully created"})
-        });
-        
-
-    } catch(error){
-        next(error);
-    };
-};
+      return res.json(data)
+    })
+  } catch (error) {
+    next(error)
+  }
+}
 
 const updateMessage = async (req, res, next) => {
-    try {
-        
-        const { id } = req.params
-        const { 
-            chat, 
-            user, 
-            message, 
-            parent 
-        } = req.body;
+  try {
+    const { id } = req.params
+    const { chat, user, message, parent } = req.body
 
-        if(!id) return res.status(400).json({ msg: "Please put a valid role_id"});
+    if (!id) return res.status(400).json({ msg: 'Please put a valid role_id' })
 
-        const MessageFind = await Message.findOneAndUpdate({id:id},{ '$set': {
+    const MessageFind = await Message.findOneAndUpdate(
+      { id: id },
+      {
+        $set: {
+          chat,
+          user,
+          message,
+          parent,
+        },
+      },
+      { upsert: true }
+    )
 
-            chat, 
-            user, 
-            message, 
-            parent 
-
-        }, 
-        },{upsert: true});
-
-        MessageFind ? res.json({ msg: "message succesfully updated"}): res.status(400).json({ msg: "Please put a valid message id"});
-
-    } catch (error) {
-        next(error);
-    };
-};
+    MessageFind
+      ? res.json({ msg: 'message succesfully updated' })
+      : res.status(400).json({ msg: 'Please put a valid message id' })
+  } catch (error) {
+    next(error)
+  }
+}
 
 const deleteMessage = async (req, res, next) => {
+  try {
+    const { id } = req.params
 
-    try{
-        const {id} = req.params
+    await Message.deleteOne(
+      {
+        id: id,
+      },
+      (err) => {
+        if (err)
+          return res
+            .status(400)
+            .json({ msg: "doesn't exist any message with this id" })
 
-        await Message.deleteOne({
-            id: id
-        }, (err) => {
-            if(err) return res.status(400).json({msg: "doesn't exist any message with this id"})
-
-            return res.json({msg: "message has been eliminated"})
-        });
-
-    } catch(error){
-        next(error);
-    };
-};
+        return res.json({ msg: 'message has been eliminated' })
+      }
+    )
+  } catch (error) {
+    next(error)
+  }
+}
 
 module.exports = {
-    get_msgByChat,
-    createMsg,
-    updateMessage,
-    deleteMessage
+  get_msgByChat,
+  createMsg,
+  updateMessage,
+  deleteMessage,
 }
