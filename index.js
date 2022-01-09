103ff8f8c4f09f944bc032572152336bc71d0345
Jose Sanchez <sanchezjoserafael25@gmail.com>
2022-01-09 11:24:35 -0400

    Fix: Corrigiendo problema de variables de entorno para hacer deploy a heroku

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const app = require('./src/app')
 const { port } = require('./src/lib/config')
-const {port_io} = require('./src/lib/config');
+const { port_io } = require('./src/lib/config')
 const { connect } = require('./src/db')
 const server = require('./src/socket.io')
-
 
 connect().then(() => {
   app.listen(port, async () => {
@@ -12,7 +11,6 @@ connect().then(() => {
 })
 
 //dejamos escuchando el server de socket.io en un puerto especifico
-server.listen(port_io, async () => {
+server.listen(port, async () => {
   console.log(`The server is running on port ${port_io}`)
 })
-
